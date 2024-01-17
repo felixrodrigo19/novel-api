02d1997a33ca34faa5e428922e2780ca76a8d117
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-01-17 09:02:40 -0300

    feat: use mux router and add all novel controller

--- routes/routes.go
+++ routes/routes.go
@@ -5,9 +5,13 @@ import (
 	"net/http"
 
 	"github.com/felixrodrigo19/rest-api-go/controllers"
+	"github.com/gorilla/mux"
 )
 
 func HandlerRequest() {
-	http.HandleFunc("/", controllers.Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/novel", controllers.AllNovels)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
