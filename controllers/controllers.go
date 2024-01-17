cc716f87e9ca53c9cd9b18e7b020d7619f305b89
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-01-17 09:27:14 -0300

    feat: filter novel by id

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/felixrodrigo19/rest-api-go/models"
+	"github.com/gorilla/mux"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -14,4 +16,15 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func AllNovels(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(models.Novels)
+}
+
+func Novel(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, novel := range models.Novels {
+		if strconv.Itoa(novel.Id) == id {
+			json.NewEncoder(w).Encode(novel)
+		}
+	}
 }
