34e1deb3d529b9d0020877704cc5fb95d7e5f866
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-01-17 09:03:16 -0300

    feat: add all novels controller

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,17 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/felixrodrigo19/rest-api-go/models"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home")
+}
+
+func AllNovels(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Novels)
 }
