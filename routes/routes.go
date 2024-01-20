f746a4ca5b253a21139c3874166417c6aa677728
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-01-20 10:45:10 -0300

    feat: add routes to authors

--- routes/routes.go
+++ routes/routes.go
@@ -11,8 +11,13 @@ import (
 func HandlerRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home).Methods("GET")
+
 	r.HandleFunc("/novel", controllers.AllNovels).Methods("GET")
 	r.HandleFunc("/novel/{id}", controllers.Novel).Methods("GET")
+	r.HandleFunc("/novel", controllers.CreateNewNovel).Methods("POST")
 
+	r.HandleFunc("/author", controllers.AllAuthors).Methods("GET")
+	r.HandleFunc("/author/{id}", controllers.Author).Methods("GET")
+	r.HandleFunc("/author", controllers.CreateNewAuthor).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
