02badd841c5912f2d3e8006be86160cfeecf7537
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-01-20 09:29:04 -0300

    feat: start DB

--- main.go
+++ main.go
@@ -3,17 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/felixrodrigo19/rest-api-go/models"
+	"github.com/felixrodrigo19/rest-api-go/database"
 	"github.com/felixrodrigo19/rest-api-go/routes"
 )
 
 func main() {
-	models.Novels = []models.Novel{
-		{Id: 1, Title: "Title 1", Description: "Description 1", Language: "Japanese",
-			Type: "Novel", Genre: models.Genre{Id: 1, Name: "Romance"},
-			Author: models.Author{Id: 1, Name: "Author 1"}, Year: 2020},
-	}
-
+	database.DBConection()
 	fmt.Println("Server On")
 	routes.HandlerRequest()
 }
