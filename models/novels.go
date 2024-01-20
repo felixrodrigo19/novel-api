265a894963b89325f7c06e176aa7b75e01d7aca2
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-01-20 09:27:38 -0300

    feat: add gorm model embedded

--- models/novels.go
+++ models/novels.go
@@ -1,16 +1,21 @@
 package models
 
+import "gorm.io/gorm"
+
 type Genre struct {
+	gorm.Model
 	Id   int    `json:"genre-id"`
 	Name string `json:"genre-name"`
 }
 
 type Author struct {
+	gorm.Model
 	Id   int    `json:"author-id"`
 	Name string `json:"author-name"`
 }
 
 type Novel struct {
+	gorm.Model
 	Id          int    `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
