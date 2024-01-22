600b5b5c3d5ceb1ad9f47822d3096af33ff51dea
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-01-22 07:05:26 -0300

    feat: use pointers

--- models/novels.go
+++ models/novels.go
@@ -4,26 +4,26 @@ import "gorm.io/gorm"
 
 type Novel struct {
 	gorm.Model
-	Id          int      `gorm:"primaryKey;autoIncrement"`
-	Title       string   `gorm:"unique;not null"`
-	Description string   `gorm:"not null"`
-	Language    string   `gorm:"not null"`
-	Type        string   `gorm:"not null"`
-	Genres      []Genre  `gorm:"many2many:novel_genres"`
-	Authors     []Author `gorm:"many2many:novel_authors"`
-	Year        int      `gorm:"not null"`
+	Id          int       `gorm:"primaryKey;autoIncrement"`
+	Title       string    `gorm:"unique;not null"`
+	Description string    `gorm:"not null"`
+	Language    string    `gorm:"not null"`
+	Type        string    `gorm:"not null"`
+	Genres      []*Genre  `gorm:"many2many:novel_genres"`
+	Authors     []*Author `gorm:"many2many:novel_authors"`
+	Year        int       `gorm:"not null"`
 }
 
 type Genre struct {
 	gorm.Model
-	Id     int     `gorm:"primaryKey;autoIncrement"`
-	Name   string  `gorm:"unique;not null"`
-	Novels []Novel `gorm:"many2many:novel_genres"`
+	Id     int      `gorm:"primaryKey;autoIncrement"`
+	Name   string   `gorm:"unique;not null"`
+	Novels []*Novel `gorm:"many2many:novel_genres"`
 }
 
 type Author struct {
 	gorm.Model
-	Id     int     `gorm:"primaryKey;autoIncrement"`
-	Name   string  `gorm:"unique;not null"`
-	Novels []Novel `gorm:"many2many:novel_authors"`
+	Id     int      `gorm:"primaryKey;autoIncrement"`
+	Name   string   `gorm:"unique;not null"`
+	Novels []*Novel `gorm:"many2many:novel_authors"`
 }
