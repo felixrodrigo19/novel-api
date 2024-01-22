878e6d5b9c94b6ff1fcf52c476e137b4e05affbb
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-01-22 07:03:23 -0300

    fix: gorm params separation

--- models/novels.go
+++ models/novels.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Novel struct {
 	gorm.Model
-	Id          int      `gorm:"primaryKey,autoIncrement"`
-	Title       string   `gorm:"unique,not null"`
+	Id          int      `gorm:"primaryKey;autoIncrement"`
+	Title       string   `gorm:"unique;not null"`
 	Description string   `gorm:"not null"`
 	Language    string   `gorm:"not null"`
 	Type        string   `gorm:"not null"`
@@ -16,14 +16,14 @@ type Novel struct {
 
 type Genre struct {
 	gorm.Model
-	Id     int     `gorm:"primaryKey,autoIncrement"`
-	Name   string  `gorm:"unique,not null"`
+	Id     int     `gorm:"primaryKey;autoIncrement"`
+	Name   string  `gorm:"unique;not null"`
 	Novels []Novel `gorm:"many2many:novel_genres"`
 }
 
 type Author struct {
 	gorm.Model
-	Id     int     `gorm:"primaryKey,autoIncrement"`
-	Name   string  `gorm:"unique,not null"`
+	Id     int     `gorm:"primaryKey;autoIncrement"`
+	Name   string  `gorm:"unique;not null"`
 	Novels []Novel `gorm:"many2many:novel_authors"`
 }
