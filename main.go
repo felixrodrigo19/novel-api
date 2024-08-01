16fb956aa952ff3a9e04e04e5d00ea0afb0df127
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-01 14:15:30 -0300

    chore: fix method name

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/felixrodrigo19/rest-api-go/database"
 	"github.com/felixrodrigo19/rest-api-go/routes"
 )
 
 func main() {
-	database.DBConection()
+	database.DBConnection()
 	fmt.Println("Server On")
 	routes.HandlerRequest()
 }
